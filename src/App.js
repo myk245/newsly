d2c0354c32c71133e3e612aca9685816ae0143f0
myk245 <myk245@nyu.edu>
2020-04-06 00:01:40 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -5,33 +5,52 @@ import articles from './articles'
 import ArticleCard from './ArticleCard'
 import ArticleItem from './ArticleItem'
 
-
 class App extends React.Component {
-
   state = {
     articles: articles,
+    displayCards: true,
+    light: true
   }
 
-  renderArticles = () => {
-    return this.state.articles.map(article => {
-      return <ArticleCard 
-              key={article.id} 
-              title={article.title}
-              url={article.url}
-              urlToImage={article.urlToImage}
-              description={article.description}
-              />
-    })
+  renderArticleCards = () => {
+    return this.state.articles.map(article => 
+      <ArticleCard
+        key={article.id}
+        title={article.title}
+        url={article.url}
+        urlToImage={article.urlToImage}
+        description={article.description}
+      />
+    )
+  }
+
+  renderArticleList = () => {
+    return this.state.articles.map(article => 
+      <ArticleItem
+        key={article.id}
+        title={article.title}
+      />
+    )
+  }
+
+  // on Click, toggle between Dark/Light & Card/List views
+  handleClick = event => {
+    this.setState({[event.target.name]: !this.state[event.target.name]})
   }
 
   render(){
     return (
-      <div className="light">
-        <button>Switch to Dark Mode</button>
-        <button>Switch to List View</button>
-        <div className="cards">
-          {this.renderArticles()}
-        </div>
+      <div className={this.state.light ? "light" : "dark"}>
+        <button name="light" onClick={this.handleClick}>{this.state.light ? "Switch to Dark Mode" : "Switch to Light Mode"}</button>
+        <button name="displayCards" onClick={this.handleClick}>{this.state.displayCards ? "Switch to List View" : "Switch to Card View"}</button>
+        {this.state.displayCards ?
+          <div className="cards">
+            {this.renderArticleCards()}
+          </div> :
+          <div className='list'>
+            {this.renderArticleList()}
+          </div>
+        }
       </div>
     );
   }
